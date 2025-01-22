a02b41f97d7e11f4b7820e309daa7f4cbfb60694
Luca Weidler <luca.w12@schule.bremen.de>
2025-01-22 11:53:28 +0100

    Projektstruktur angepasst

--- src/app/components/employee-list/employee-list.component.ts
+++ src/app/components/employee-list/employee-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Observable, of} from "rxjs";
-import {Employee} from "../Employee";
+import {Employee} from "../../model/Employee";
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 
 @Component({
