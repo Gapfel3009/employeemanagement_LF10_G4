acc12152033134a951ac66264b2eb38731e130ac
J.Ketzler <j.ketzler@affinis.de>
2025-01-28 08:22:52 +0100

    http interceptor gebaut muss pushen zum testen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
-import {HttpClientModule} from "@angular/common/http";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { EmployeeListComponent } from './components/employee-list/employee-list.component';
 import { EditEmployeeComponent } from './components/edit-employee/edit-employee.component';
@@ -10,6 +10,7 @@ import { ShowEmployeeComponent } from './components/show-employee/show-employee.
 import { NewEmployeeComponent } from './components/new-employee/new-employee.component';
 import {SearchbarComponent} from "./components/searchbar/searchbar.component";
 import { HeaderComponent } from './components/header/header.component';
+import {JwtInterceptor} from "./jwt.interceptor";
 
 @NgModule({
   declarations: [
@@ -27,7 +28,9 @@ import { HeaderComponent } from './components/header/header.component';
     HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true},
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
